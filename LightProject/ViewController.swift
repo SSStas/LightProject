be951555078d37f29dc1e067f9cb1581cf98915e
student <student@mail.el-ts.ru>
2017-12-10 11:23:21 +0300

    Changed pressedButton() name

--- LightProject/ViewController.swift
+++ LightProject/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     var light = 0
     
-    @IBAction func PressButton(_ sender: Any) {
+    @IBAction func pressButton(_ sender: Any) {
         light = (light == 0) ? 1 : (light == 1) ? 2 : 0
         view.backgroundColor = (light == 0) ? .white : (light == 1) ? .black : .gray
     }
